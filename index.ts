4d70bb1ffa8c9c3762a239fb7a7064b2e4d09e94
SyedEbad902 <ebad3088@gmail.com>
2024-03-21 01:11:04 +0500

    Simple calculator using typscript

---
+++ index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter 1st number",
+    type: "number",
+    name: "firstNum",
+  },
+  {
+    message: "Enter 2nd number",
+    type: "number",
+    name: "secondNum",
+  },
+  {
+    message: "Select operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(answer.firstNum + answer.secondNum);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNum - answer.secondNum);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNum * answer.secondNum);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNum / answer.secondNum);
+}
